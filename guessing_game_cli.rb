e2e8ce3388a97bc4734a79caa940bb8aaa390dcb
Stanley Lee <sachih@live.com>
2020-01-01 21:56:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
     puts "Goodbye!"
   elsif input != computer_number
     puts "Sorry! The computer guessed #{computer_number}."
-  elsif input == computer_number
+  elsif input.to_i == computer_number
     puts "You guessed the correct number!"
   end
 
