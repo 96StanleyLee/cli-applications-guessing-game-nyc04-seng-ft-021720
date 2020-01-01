2ce0610af0915ebbdb97c0168f3e39fb03bcef10
Stanley Lee <sachih@live.com>
2020-01-01 21:58:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,10 +3,9 @@ def run_guessing_game
   puts "Please enter the number you think the computer has chosen"
   computer_number = rand(6) + 1
   input = gets.chomp
-
+  computer_number = computer_number.to_i
   if input == 'exit'
     puts "Goodbye!"
-    break
   elsif input != computer_number
     puts "Sorry! The computer guessed #{computer_number}."
   elsif input.to_i == computer_number
