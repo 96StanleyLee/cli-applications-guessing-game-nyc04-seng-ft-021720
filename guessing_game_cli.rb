40d98793d9020360c6f00bf1ff8f086180c5e686
Stanley Lee <sachih@live.com>
2020-01-01 21:47:32 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 def run_guessing_game
-  computer_number = rand(6) + 1
   puts "Please enter the number you think the computer has chosen"
+  computer_number = rand(6) + 1
   input = gets.chomp
 
   if input == computer_number
